b93fe527bfef8a20001f02af32d9d9868ce5f4c9
0hmixx <bm012100@gmail.com>
2026-08-26 06:35:14 +0800

    Phase 1-6: Full UI/UX redesign with new color theme
    - Complete CSS overhaul with lavender/purple color scheme
    - Redesigned Home, Recipes, Ingredients, Expenses sections
    - New ingredient cards: clean name + price list, tap to expand edit/delete
    - Fixed bottom nav active state visibility (text was white-on-white)
    - Removed ripple effect from ingredient cards (was blocking clicks)
    - Fixed cost calc fallback for purchaseQuantity=0
    - Added escapeHtml helper for XSS prevention
    - Fixed showModal to close existing modals first
    - Fixed recipe search empty state distinction
    - Replaced hardcoded dark mode colors with CSS variables
    - All 44 buttons verified working
    - 3764 lines, balanced braces/parens/brackets, 30 functions

--- app/src/main/java/com/cooopal/app/LauncherActivity.java
+++ app/src/main/java/com/cooopal/app/LauncherActivity.java
@@ -1,54 +1,76 @@
-/*
- * Copyright 2020 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.cooopal.app;
 
-import android.content.pm.ActivityInfo;
-import android.net.Uri;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
+public class LauncherActivity extends Activity {
 
-
-public class LauncherActivity
-        extends com.google.androidbrowserhelper.trusted.LauncherActivity {
-    
-
-    
+    private WebView webView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Setting an orientation crashes the app due to the transparent background on Android 8.0
-        // Oreo and below. We only set the orientation on Oreo and above. This only affects the
-        // splash screen and Chrome will still respect the orientation.
-        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
-        } else {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(getResources().getIdentifier("activity_main", "layout", getPackageName()));
+
+        webView = findViewById(getResources().getIdentifier("webview", "id", getPackageName()));
+        progressBar = findViewById(getResources().getIdentifier("progressBar", "id", getPackageName()));
+
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#D4764E"));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setNavigationBarColor(Color.BLACK);
         }
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress < 100) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        webView.setBackgroundColor(Color.WHITE);
+        webView.loadUrl("https://0hmixx.github.io/CooPal/index.html");
     }
 
     @Override
-    protected Uri getLaunchingUrl() {
-        // Get the original launch Url.
-        Uri uri = super.getLaunchingUrl();
-
-        
-
-        return uri;
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
